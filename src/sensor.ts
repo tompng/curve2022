b11f5fbf23759615a74221c912e612a840b159ca
tompng <tomoyapenguin@gmail.com>
2022-12-25 02:57:13 +0900

    smooth-gravity

--- src/sensor.ts
+++ src/sensor.ts
@@ -1,9 +1,15 @@
 export class GravitySensor {
   available = false
   gravity = { x: 0, y: 0, z: 0 }
-  referenceGravity = { x: 0, y: 0, z: 0 }
+  referenceGravity = { x: 0, y: -1, z: 1 }
   initialized = false
   button: HTMLElement
+  smoothGravity = {
+    x: 0, y: 0, z: 0,
+    ax: 0, ay: 0, az: 0,
+    bx: 0, by: 0, bz: 0,
+    cx: 0, cy: 0, cz: 0,
+  }
   constructor() {
     this.button = this.createButton()
     this.button.onclick = () => {
@@ -33,10 +39,27 @@ export class GravitySensor {
       this.gravity.x = scale * (x * cos - y * sin)
       this.gravity.y = scale * (x * sin + y * cos)
       this.gravity.z = z * scale
+      const ratioA = 0.9
+      const ratioB = ratioA ** 2
+      const ratioC = ratioA ** 3
+      this.smoothGravity.ax = (this.smoothGravity.ax + this.gravity.x) * ratioA
+      this.smoothGravity.ay = (this.smoothGravity.ay + this.gravity.y) * ratioA
+      this.smoothGravity.az = (this.smoothGravity.az + this.gravity.z) * ratioA
+      this.smoothGravity.bx = (this.smoothGravity.bx + this.gravity.x) * ratioB
+      this.smoothGravity.by = (this.smoothGravity.by + this.gravity.y) * ratioB
+      this.smoothGravity.bz = (this.smoothGravity.bz + this.gravity.z) * ratioB
+      this.smoothGravity.cx = (this.smoothGravity.cx + this.gravity.x) * ratioC
+      this.smoothGravity.cy = (this.smoothGravity.cy + this.gravity.y) * ratioC
+      this.smoothGravity.cz = (this.smoothGravity.cz + this.gravity.z) * ratioC
+      const smoothScale = 1 / (1 - ratioA) - 2 / (1 - ratioB) + 1 / (1 - ratioC)
+      this.smoothGravity.x = (this.smoothGravity.ax - 2 * this.smoothGravity.bx + this.smoothGravity.cx) / smoothScale
+      this.smoothGravity.y = (this.smoothGravity.ay - 2 * this.smoothGravity.by + this.smoothGravity.cy) / smoothScale
+      this.smoothGravity.z = (this.smoothGravity.az - 2 * this.smoothGravity.bz + this.smoothGravity.cz) / smoothScale
     })
   }
   upgradeButton() {
     this.button.textContent = 'Reset'
+    this.button.style.display = 'block'
     this.button.onclick = () => {
       this.referenceGravity = { ...this.gravity }
     }
